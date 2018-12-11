26601fd85e1cd07607e15e819df6e412ef944705
Beejay Morgan <beejay@simplecast.com>
2018-12-11 12:32:36 -0500

    ⛓ with linked lists, it finishes in 6s rather than 5.5h

--- day-09/try2/index.js
+++ day-09/try2/index.js
@@ -1,6 +1,6 @@
+// New and improved!! Linked Lists make short work!
 const start = Date.now()
 const players = 441
-// Be patient... this took over 5.5 hours to run.
 const marbles = 71032 * 100
 
 let firstMarble = {
@@ -8,6 +8,7 @@ let firstMarble = {
   p: null,
   n: null
 }
+firstMarble.n = firstMarble
 let currentMarble = 1
 let currentPlayer = 1
 
@@ -15,93 +16,58 @@ let scores = {}
 
 console.log(players, marbles)
 
-// while (currentMarble < marbles) {
-//   let player = 0
-//   // console.log(currentMarble, marbles)
-//   for (player; player < players; player++) {
-//     if (currentMarble > marbles) { continue }
-//     if (currentMarble % 23 === 0) {
-//       // console.log('found a 23')
-//       scores[player] = scores[player] || 0
-//       scores[player] += currentMarble
-//       let newIndex = currentPosition - 7
-//       if (newIndex < 0) {
-//         newIndex = circle.length + newIndex
-//       }
-//       currentPosition = newIndex
-//       let removed = circle.splice(newIndex, 1)
-//       // console.log(`plus ${removed} to the score of player ${player + 1}`)
-//       scores[player] = scores[player] * 1 + removed * 1
-//       // console.log(player, currentMarble, currentPosition, scores)
-//       currentMarble++
-//       continue
-//     }
-//     let newIndex = currentPosition + 2
-//     if (newIndex > circle.length) {
-//       newIndex = newIndex - circle.length
-//     }
-//     if (newIndex === circle.length) {
-//       circle.push(currentMarble)
-//     } else {
-//       circle.splice(newIndex, 0, currentMarble)
-//     }
-//     currentPosition = newIndex
-//     // console.log(player, currentMarble, currentPosition)
-//     if (currentMarble % 10000 === 0) {
-//       console.log('on marble ' + currentMarble, Date.now() - start)
-//     }
-//     currentMarble++
-//   }
-// }
-
 function playMarble (currentPosition) {
   if (currentMarble % 1000000 === 0) {
-    console.log(currentMarble, marbles, currentPlayer)
+    console.log(currentMarble, currentPosition.v, currentPosition.n.v, currentPosition.p.v)
   }
   if (currentMarble > marbles) {
     printResults()
     return
   }
-  if (currentMarble % 23 === 0) {
+  if (currentPlayer > players) { currentPlayer = 1 }
+  if (currentMarble > 0 && currentMarble % 23 === 0) {
+    // console.log('scoring', currentMarble)
     scores[currentPlayer] = scores[currentPlayer] || 0
     scores[currentPlayer] += currentMarble
-    let temp = currentPosition
-    for (let p=0;p<7;p++) {
-      temp = temp.p
-      if (!temp) {
-        temp = firstMarble
-        while (temp.n) {
-          temp = temp.n
-        }
-      }
-    }
-    scores[currentPlayer] += temp.v
-    temp.p.n = temp.n
-    temp.n.p = temp.p
+    let newPos = findMarble(currentPosition, -7)
+    // console.log('scoring', newPos.v)
+    scores[currentPlayer] += newPos.v
+    newPos.p.n = newPos.n
+    newPos.n.p = newPos.p
     currentMarble += 1
-    currentPlayer = currentPlayer === players ? 1 : currentPlayer + 1
-    process.nextTick(() => playMarble(temp.n))
+    currentPlayer += 1
+    process.nextTick(() => playMarble(newPos.n))
     return
   }
-  // console.log(currentMarble, currentPosition)
+  let newPos = findMarble(currentPosition, 1)
+  // console.log('found', newPos.v)
+  const newMarble = makeMarble(currentMarble, newPos, newPos.n)
+  if (newPos.n) { newPos.n.p = newMarble }
+  // console.log('newPos.n.p', newPos.n.p.v)
+  newPos.n = newMarble
+  newMarble.p = newPos
+  // console.log(newMarble.v, 'between', newMarble.p.v, newMarble.n.v)
   currentMarble += 1
-  currentPlayer = currentPlayer === players ? 1 : currentPlayer + 1
-  let temp = currentPosition
-  for (let p=0; p < 2; p++) {
-    temp = temp.n || firstMarble
-  }
-  let newMarble = {
-    v: currentMarble,
-    p: temp,
-    n: null
-  }
-  temp.n = newMarble
+  currentPlayer += 1
   process.nextTick(() => playMarble(newMarble))
+}
+
+function makeMarble (v, p, n) {
+  return {v, p, n}
+}
+
+function findMarble (current, move=0) {
+  if (move === 0) { return current }
+  // console.log('finding', current.v, move)
+  if (!current) {
+    return move > 0 ? firstMarble : null
+  }
+  return move < 0 ? findMarble(current.p, move + 1) : findMarble(current.n, move - 1)
 }
 
 function printResults () {
   // console.log('scores: ', scores)
-  console.log(currentMarble)
+  // console.log(currentMarble)
   const highest = Object.keys(scores).reduce((a, v) => {
     return Math.max(a, scores[v])
   }, 0)
